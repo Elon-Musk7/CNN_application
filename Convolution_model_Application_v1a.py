77b4ca4a7230677ee8f392bc695ac273feba5286
Elon-musk7 <xuchun.ren777@gmail.com>
2020-08-15 18:31:35 -0300

    Initialize parameters

---
+++ Convolution_model_Application_v1a.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 14 20:26:58 2020
+
+@author: ren
+"""
+
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+#import tensorflow as tf
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from cnn_utils import *
+
+#%mathplotlib inline
+np.random.seed(1)
+
+# Loading the data (signs)
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+
+# Example of a picture
+index = 6
+plt.imshow(X_train_orig[index])
+print ("y = " + str(np.squeeze(Y_train_orig[:, index])))
+
+
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+Y_train = convert_to_one_hot(Y_train_orig, 6).T
+Y_test = convert_to_one_hot(Y_test_orig, 6).T
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+conv_layers = {}
+
+
+# GRADED FUNCTION: create_placeholders
+
+def create_placeholders(n_H0, n_W0, n_C0, n_y):
+    """
+    Creates the placeholders for the tensorflow session.
+    
+    Arguments:
+    n_H0 -- scalar, height of an input image
+    n_W0 -- scalar, width of an input image
+    n_C0 -- scalar, number of channels of the input
+    n_y -- scalar, number of classes
+        
+    Returns:
+    X -- placeholder for the data input, of shape [None, n_H0, n_W0, n_C0] and dtype "float"
+    Y -- placeholder for the input labels, of shape [None, n_y] and dtype "float"
+    """
+
+    ### START CODE HERE ### (≈2 lines)
+    X = tf.placeholder("float",[None, n_H0, n_W0, n_C0])
+    Y = tf.placeholder("float",[None, n_y])
+    ### END CODE HERE ###
+    
+    return X, Y
+
+
+X, Y = create_placeholders(64, 64, 3, 6)
+print ("X = " + str(X))
+print ("Y = " + str(Y))
+
+
+
+# GRADED FUNCTION: initialize_parameters
+
+def initialize_parameters():
+    """
+    Initializes weight parameters to build a neural network with tensorflow. The shapes are:
+                        W1 : [4, 4, 3, 8]
+                        W2 : [2, 2, 8, 16]
+    Note that we will hard code the shape values in the function to make the grading simpler.
+    Normally, functions should take values as inputs rather than hard coding.
+    Returns:
+    parameters -- a dictionary of tensors containing W1, W2
+    """
+    
+    tf.set_random_seed(1)                              # so that your "random" numbers match ours
+        
+    ### START CODE HERE ### (approx. 2 lines of code)
+    W1 = tf.get_variable("W",[4,4,3,18],initializer=tf.contrib.layers.xavier_initializer(seed=0))
+    W2 = tf.get_variable("W2",[2,2,8,16], initializer=tf.contrib.layers.xavier_initializer(seed=0))
+    ### END CODE HERE ###
+
+    parameters = {"W1": W1,
+                  "W2": W2}
+    
+    return parameters
+
+
+tf.reset_default_graph()
+with tf.Session() as sess_test:
+    parameters = initialize_parameters()
+    init = tf.global_variables_initializer()
+    sess_test.run(init)
+    print("W1[1,1,1] = \n" + str(parameters["W1"].eval()[1,1,1]))
+    print("W1.shape: " + str(parameters["W1"].shape))
+    print("\n")
+    print("W2[1,1,1] = \n" + str(parameters["W2"].eval()[1,1,1]))
+    print("W2.shape: " + str(parameters["W2"].shape))
